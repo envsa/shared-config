fe50d94162c408a11e34d838f7da78b8a6ea2395
Liam Rella <liam.rella@gmail.com>
2024-09-16 12:57:56 +0930

    remove remarklint

--- packages/remark-config/src/cli.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { buildCommands } from '$root/src/command-builder.ts';
-
-await buildCommands('remark-config', '[remarklint]', 'blue', {
-  init: {},
-  printConfig: {},
-});
