ecfead46055113320c901dd4b81c1ad008dd0baa
Liam Rella <liam.rella@gmail.com>
2025-02-17 23:06:21 +1030

    chore: update cspell config

--- cspell.config.js
+++ cspell.config.js
@@ -1,5 +1,6 @@
 import { cspellConfig } from '@envsa/cspell-config';
 
 export default cspellConfig({
-  ignorePaths: ['/packages/*/init/*.json', '/packages/*/bin'],
+  ignorePaths: ['/packages/*/init/*.json', '/packages/*/bin', './test/fixtures/input', './test/fixtures/output-fixed-auto', './packages/eslint-config/src/typegen.d.ts'],
+  words: []
 });
